ea8fc1530140a715b8a9e45c5fc5aa72ffce64f7
Ded444 <123232594+Ded444@users.noreply.github.com>
2023-02-17 21:06:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _123
+{
+    internal class Program
+     {
+        static void Main()
+        {
+            int a;
+            Console.WriteLine("Vvedite a ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if((a % 3 == 0)&&(a % 5 == 0))
+            {
+                Console.WriteLine("fezzbuzz");
+            }
+            else if(a % 3 == 0) {
+                Console.WriteLine("fizz");
+            }
+            else if (a % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+        }
+    }
+}
